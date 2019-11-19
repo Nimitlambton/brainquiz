c8d06cf722f9e6ae5cc56a60e28bb6bf79ca145b
JASI <jasimac@JASIs-Mac.local>
2019-11-19 15:18:27 -0800

    second

--- quizApp/ViewController.swift
+++ quizApp/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var question: UITextView!
     
+    @IBOutlet weak var option1: UILabel!
+    
+    @IBOutlet weak var option2: UILabel!
+    
+    
+    @IBOutlet weak var option3: UILabel!
+    
+    @IBOutlet weak var option4: UILabel!
     
     
     override func viewDidLoad() {
@@ -40,9 +48,19 @@ let path = Bundle.main.path(forResource: "questionList", ofType: "plist")
         
         let data = dict?.object(forKey: "questions") as! [[String]]
         let try1 =  data.randomElement()
-        print(try1![1])
+    
+        print(try1)
+
+        question.text = try1![0]
         
-        
+        option1.text = try1![2]
+        option2.text = try1![2]
+        option3.text = try1![3]
+        option4.text = try1![4]
+      
+        img.image = UIImage(named: try1![5])
+
+
     }
     
     
